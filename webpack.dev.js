1782a2cc06d1a0cde77a5d76e0b7ab196b9e78d9
Esteban Pena <estpena1@publicisgroupe.net>
2022-08-25 12:57:39 -0500

    fixed issue on dev env, dev server was not reloading properly

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,9 +11,7 @@ module.exports = merge(common, {
 		clean: true
 	},
 	devServer: {
-		static: {
-			directory: path.join(__dirname, 'dist')
-		},
+		static: ['src/template.html'],
 		devMiddleware: {
 			writeToDisk: false
 		},
