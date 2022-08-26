55321af33a2b14693edc68fbfb1affb5ecc28b2b
Esteban Pena <estpena1@publicisgroupe.net>
2022-08-26 14:54:32 -0500

    final eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		"plugin:@typescript-eslint/recommended",
-		"prettier"
-	],
-	overrides: [],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-	},
-	plugins: ["@typescript-eslint"],
-	rules: {
-		indent: ["error", "tab"],
-		"linebreak-style": 0,
-		quotes: ["error", "double"],
-		semi: ["error", "never"],
-		"@typescript-eslint/no-var-requires": 0,
-		"@typescript-eslint/ban-ts-comment": "off",
-	},
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // indent: ["error", "tab"],
+    indent: ['error', 2],
+    'linebreak-style': 0,
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
 }
