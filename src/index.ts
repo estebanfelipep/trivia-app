d7c9e5116b53bdc52373dedf870ff3702e261233
Esteban Pena <estpena1@publicisgroupe.net>
2022-09-08 13:45:46 -0500

    home done

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,12 @@ if (module.hot) {
   // @ts-ignore
   module.hot.accept()
 }
+
+const chooseCatForm = document.querySelector('#chooseCat')
+
+chooseCatForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log('test')
+})
+
 console.log('log from index.ts!')
